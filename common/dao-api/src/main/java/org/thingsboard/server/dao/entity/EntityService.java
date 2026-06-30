3caf6df5b624ab0b1cb6cd5610aeef517da7a5eb
Julien <146082906+jujuyahtec77@users.noreply.github.com>
2026-06-30 14:33:04 +0200

    feat(security/scope): surcharges EntityService/Dao scopees (bypass EDQS)

--- common/dao-api/src/main/java/org/thingsboard/server/dao/entity/EntityService.java
+++ common/dao-api/src/main/java/org/thingsboard/server/dao/entity/EntityService.java
@@ -24,13 +24,16 @@ import org.thingsboard.server.common.data.id.HasId;
 import org.thingsboard.server.common.data.id.NameLabelAndCustomerDetails;
 import org.thingsboard.server.common.data.id.TenantId;
 import org.thingsboard.server.common.data.page.PageData;
+import org.thingsboard.server.common.data.permission.CustomerScopeMode;
 import org.thingsboard.server.common.data.query.EntityCountQuery;
 import org.thingsboard.server.common.data.query.EntityData;
 import org.thingsboard.server.common.data.query.EntityDataQuery;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.UUID;
 
 public interface EntityService {
 
@@ -53,5 +56,9 @@ public interface EntityService {
     PageData<EntityData> findEntityDataByQuery(TenantId tenantId, CustomerId customerId, EntityDataQuery query);
 
     ListenableFuture<PageData<EntityData>> findEntityDataByQueryAsync(TenantId tenantId, CustomerId customerId, EntityDataQuery query);
+
+    long countEntitiesByQueryScoped(TenantId tenantId, List<UUID> customerIds, CustomerScopeMode scopeMode, EntityCountQuery query);
+
+    PageData<EntityData> findEntityDataByQueryScoped(TenantId tenantId, List<UUID> customerIds, CustomerScopeMode scopeMode, EntityDataQuery query);
 
 }
